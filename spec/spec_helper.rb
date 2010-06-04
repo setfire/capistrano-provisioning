75bfef43deb7b933e75bf432527ff5a7f742695f
Sam Phillips <sam@samdanavia.com>
2010-06-05 00:42:33 +0100

    Basic bootstrapping

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
+require 'capistrano'
 require 'capistrano-provisioning'
 require 'spec'
 require 'spec/autorun'
