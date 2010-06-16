82081b76bdcdc5f24a587b7aa9c583ff1f9125aa
Sam Phillips <sam@samdanavia.com>
2010-06-16 16:08:24 +0100

    Removing pending specs that are covered elsewhere

--- spec/recipes_spec.rb
+++ spec/recipes_spec.rb
@@ -3,29 +3,15 @@ require 'spec_helper'
 # TODO: Not sure how to test this yet
 
 describe "Recipes" do  
-  it "should create an :all task which loads all servers in that namespace"
+  context "namespace" do
+    it "should create an :all task which loads all servers in that namespace"    
+  end
 
   context "cluster definition" do
-    it "should take a list of servers without a block"
-    
     it "should create a task to set the servers"
     
     it "should create a task to bootstrap the servers"
     
     it "should create a task to install users on the servers"
-    
-    context "servers" do
-      it "should take a list of servers"
-  
-      it "should correctly set the current servers"
-
-      it "should raise an error if no servers are specifed"
-    end
-    
-    context "users" do
-      it "should be passed into the cluster definition"
-      
-      it "should have the option of belonging to a group"
-    end
   end
 end
